ee3ce45c301222f95adf284798b840b1e08df010
mlycode <m.ly@live.co.uk>
2018-10-20 14:24:12 +0100

    add styling with bootstrap

--- src/App.js
+++ src/App.js
@@ -48,15 +48,27 @@ class App extends Component {
   render() {
     return (
       <Fragment>
-        <Titles />
-        <Form getWeather={this.getWeather}/>
-        <Weather
-          temperature={this.state.temperature}
-          city={this.state.city}
-          country={this.state.country}
-          humidity={this.state.humidity}
-          description={this.state.description}
-          error={this.state.error} />
+        <div className="wrapper">
+          <div className="main">
+            <div className="container">
+              <div className="row">
+                <div className="col-xs-5 title-container">
+                  <Titles />
+                </div>
+                <div className="col-xs-7 form-container">
+                  <Form getWeather={this.getWeather}/>
+                  <Weather
+                    temperature={this.state.temperature}
+                    city={this.state.city}
+                    country={this.state.country}
+                    humidity={this.state.humidity}
+                    description={this.state.description}
+                    error={this.state.error} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </Fragment>
     );
   }
